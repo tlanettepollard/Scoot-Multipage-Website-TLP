76928bc3d8ee4f193d91b7fc7e8342520a1b00e0
T. Lanette <TLPollard4516@gmail.com>
2022-03-31 22:11:14 -0400

    Updates: sidebar
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ main.js
@@ -0,0 +1,9 @@
+// Sidebar
+
+function openNav() {
+    document.getElementById('mobileSidebar').style.width = '250px';
+}
+
+function closeNav() {
+    document.getElementById('mobileSidebar').style.width = '0';
+}
