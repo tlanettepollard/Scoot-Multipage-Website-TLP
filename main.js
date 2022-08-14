2b3bd68c02394a82c514b44609812d8529d0f677
T. Lanette <TLPollard4516@gmail.com>
2022-08-13 20:17:54 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- main.js
+++ main.js
@@ -10,9 +10,16 @@ function closeNav() {
 
 // Accordion
 const accordion = document.getElementsByClassName('faq__container');
-
+let i;
 for (i = 0; i < accordion.length; i++) {
 	accordion[i].addEventListener('click', function () {
 		this.classList.toggle('active');
+
+		let panel = this.nextElementSibling;
+		if (panel.style.display === 'block') {
+			panel.style.display = 'none';
+		} else {
+			panel.style.display = 'block';
+		}
 	});
 }
